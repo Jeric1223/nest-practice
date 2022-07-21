063d46e0713c135909feb86949f57897144c7898
JaeHyun <soehd0889@cntech.pro>
2022-07-21 15:03:56 +0900

    git 파일 재배치

---
+++ nomardCoder-nestJs/dist/movies/movies.controller.d.ts
@@ -0,0 +1,13 @@
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+export declare class MoviesController {
+    private readonly moviesService;
+    constructor(moviesService: MoviesService);
+    getAll(): Movie[];
+    getOne(id: number): Movie;
+    create(movieData: CreateMovieDto): void;
+    deleteMovie(movieId: number): void;
+    patch(movieId: number, updateData: UpdateMovieDto): void;
+}
