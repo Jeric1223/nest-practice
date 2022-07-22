76807c09ed3e1ba9cd0f53afc6beb99f812eaebb
JaeHyun <soehd0889@cntech.pro>
2022-07-22 13:27:41 +0900

    레퍼지토리 코드 작성

--- youtube-nestJs/nestjs-test/src/boards/board-repository.ts
+++ youtube-nestJs/nestjs-test/src/boards/board-repository.ts
@@ -1,5 +1,36 @@
+import { NotFoundException, NotImplementedException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+export class BoardRepository extends Repository<Board> {
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    const { title, description } = createBoardDto;
+
+    const board = this.create({
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    });
+
+    await this.save(board);
+    return board;
+  }
+  async getBoardById(id: number): Promise<Board> {
+    const found = await this.findOne(id);
+
+    if (!found) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    return found;
+  }
+  async deleteBoard(id: number): Promise<void> {
+    const result = await this.delete(id); //바로 삭제 됨
+    if (result.affected === 0) {
+      throw new NotFoundException(`Cant find Board with id ${id}`);
+    }
+  }
+}
--- youtube-nestJs/nestjs-test/src/boards/boards.controller.ts
+++ youtube-nestJs/nestjs-test/src/boards/boards.controller.ts
@@ -4,12 +4,14 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BaordStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -18,32 +20,46 @@ import { BaordStatusValidationPipe } from './pipes/board-status-validation.pipe'
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
-  getAllBoard(): Board[] {
-    return this.boardsService.getAllBoards();
-  }
+  // @Get()
+  // getAllBoard(): Board[] {
+  //   return this.boardsService.getAllBoards();
+  // }
 
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  //   return this.boardsService.createBoard(createBoardDto);
+  // }
   @Post()
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardsService.createBoard(createBoardDto);
   }
 
+  // @Get(':id')
+  // getBoardById(@Param('id') id: string): Board {
+  //   return this.boardsService.getBoardById(id);
+  // }
+
   @Get(':id')
-  getBoardById(@Param('id') id: string): Board {
+  getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardsService.getBoardById(id);
   }
 
+  // @Delete(':id')
+  // deleteBoard(@Param('id') id: string): void {
+  //   return this.boardsService.deleteBoard(id);
+  // }
   @Delete(':id')
-  deleteBoard(@Param('id') id: string): void {
-    return this.boardsService.deleteBoard(id);
+  deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
+    return this.boardsService.deleteBaord(id);
   }
 
-  @Patch(':id/status')
-  updateBoardStatus(
-    @Param('id') id: string,
-    @Body('status', BaordStatusValidationPipe) status: BoardStatus,
-  ): Board {
-    return this.boardsService.updateBoardStatus(id, status);
-  }
+  // @Patch(':id/status')
+  // updateBoardStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', BaordStatusValidationPipe) status: BoardStatus,
+  // ): Board {
+  //   return this.boardsService.updateBoardStatus(id, status);
+  // }
 }
