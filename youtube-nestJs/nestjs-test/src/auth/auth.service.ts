cf9404e6bab4d93a58b62cc40a870da4d8f69b53
JaeHyun <soehd0889@cntech.pro>
2022-07-23 18:29:22 +0900

    jwt 기능 구현, 커스텀 데코레이터 만들기

--- youtube-nestJs/nestjs-test/src/auth/auth.service.ts
+++ youtube-nestJs/nestjs-test/src/auth/auth.service.ts
@@ -9,11 +9,13 @@ import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
   async createUser(authCredentialsDto: AuthCredentialsDto) {
@@ -41,12 +43,18 @@ export class AuthService {
     }
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOne({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      // 유저 토큰 생성 (Secret + Payload)
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken: accessToken };
     } else {
       throw new UnauthorizedException('login failed');
     }
