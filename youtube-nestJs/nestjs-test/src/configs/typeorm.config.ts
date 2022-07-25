b142b788e11afc849a3da917e3de1e20c834605e
JaeHyun <soehd0889@cntech.pro>
2022-07-25 14:22:35 +0900

    .

--- youtube-nestJs/nestjs-test/src/configs/typeorm.config.ts
+++ youtube-nestJs/nestjs-test/src/configs/typeorm.config.ts
@@ -2,11 +2,17 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'board-app',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
+
+/* host: process.env.DB_HOST,
+port: +process.env.DB_PORT,
+username: process.env.DB_USERNAME,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_DATABASE, */
